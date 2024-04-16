a3d4d33ae8798e7e98619c8612afea197831044d
Mikkel Silvasen <mikkelkp360@gmail.com>
2024-04-16 13:33:53 +0200

    Added hnalders and modules

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from "morgan";
+import router from "./router";
+import { protect } from "./modules/auth";
+import { createNewUser, signIn } from "./handlers/user";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res, next) => {
+  setTimeout(() => {
+    next(new Error("hello"));
+  }, 1);
+});
+
+app.use("/api", protect, router);
+app.post("/user", createNewUser);
+app.post("/signin", signIn);
+
+app.use((err, req, res, next) => {
+  if (err.type === "auth") {
+    res.status(401).json({ message: "unauthorized" });
+  } else if (err.type === "input") {
+    res.status(400).json({ message: "invalid input" });
+  } else {
+    res.status(500).json({ message: "oops, thats on us" });
+  }
+});
+
+export default app;
